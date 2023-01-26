339aa8a919e661bc3853f6cfae1fb1a6e9b2e790
Shahid Arshad <shahid.arshad@devsinc.com>
2023-01-26 19:19:25 +0500

    Initial Commit

---
+++ RockPaperScissor/RockPaperScissorApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperScissorApp.swift
+//  RockPaperScissor
+//
+//  Created by Dev on 26/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct RockPaperScissorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
